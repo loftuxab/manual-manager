63311ea5a4c1d53d19d11c91bc2a1d9d6d7b128e
Peter Löfgren <peter@loftux.se>
2016-03-01 19:13:21 +0100

    Move to SDK.

---
+++ run.bat
@@ -0,0 +1,19 @@
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::      Dev environment startup script for Alfresco Community     ::
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+@echo off
+
+set springloadedfile=%HOME%\.m2\repository\org\springframework\springloaded\1.2.5.RELEASE\springloaded-1.2.5.RELEASE.jar
+
+if not exist %springloadedfile% (
+  mvn validate -Psetup
+)
+
+:: Use these settings if you're using JDK7
+:: set MAVEN_OPTS=-javaagent:"%springloadedfile%" -noverify -Xms256m -Xmx2G -XX:PermSize=300m
+
+:: Spring loaded does not work very well with 5.1 at the moment, breaks the H2 db after first run and then restart
+:: set MAVEN_OPTS=-javaagent:"%springloadedfile%" -noverify -Xms256m -Xmx2G
+set MAVEN_OPTS=-noverify -Xms256m -Xmx2G
+
+mvn clean install -Prun -nsu
---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Downloads the spring-loaded lib if not existing and runs the full all-in-one
+# (Alfresco + Share + Solr) using the runner project
+springloadedfile=~/.m2/repository/org/springframework/springloaded/1.2.5.RELEASE/springloaded-1.2.5.RELEASE.jar
+
+if [ ! -f $springloadedfile ]; then
+mvn validate -Psetup
+fi
+
+# Use these settings if you're using JDK7
+# MAVEN_OPTS="-javaagent:$springloadedfile -noverify -Xms256m -Xmx2G -XX:PermSize=300m" mvn install -Prun
+
+# Spring loaded does not work very well with 5.1 at the moment, breaks the H2 db after first run and then restart
+#MAVEN_OPTS="-javaagent:$springloadedfile -noverify -Xms256m -Xmx2G" mvn clean install -Prun
+MAVEN_OPTS="-noverify -Xms256m -Xmx2G" mvn clean install -Prun
